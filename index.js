fb979ad7a403ab733b323111d25966a5d85af358
Anusha <anushasunil71201@gmail.com>
2021-09-23 01:36:19 +0530

    refactor: added a function to check if prime

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 var userInputYear = document.querySelector("#userInputYear");
 var btnCheck = document.querySelector(".check");
 var outputMessage = document.querySelector(".result");
+
+var isPrime = true;
+
+function isPrimeNum(inputNum)
+{
+	if(inputNum > 1)
+	{
+		for(var i = 2; i < inputNum; i++)
+		{
+			if(inputNum % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+
+		outputResult(isPrime);
+	}
+	else if(inputNum == 1 || inputNum == 0)
+	{
+		console.log("neither prime nor composite");
+	}
+	else {
+		console.log("enter a valid input");
+	}
+}
